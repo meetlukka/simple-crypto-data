86d058088513d9aa1fbde28c6e19c32c3a3bf1b3
meetlukka <meetlukka@users.noreply.github.com>
2021-06-17 02:59:33 +0530

    api setup

--- fetchapi.js
+++ fetchapi.js
@@ -1,20 +1,35 @@
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-function fetchData(){
 
-var response = axios({
-  method: 'get',
-  url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-  headers: {
-      X-CMC_PRO_API_KEY:'90008643-8ea8-4d30-bf6d-e3cfaec56f1a',
-      Accept:'application/json',
-  }, 
-  data: {
-    start:'1',
-    limit:'5000',
-    convert:'USD'
-  }
-});
-    console.log(response)
+function fetchData(){
+    console.log("fetch call")
+
+    
+fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=10&convert=USD&CMC_PRO_API_KEY=90008643-8ea8-4d30-bf6d-e3cfaec56f1a")
+.then((res)=>{
+    return res.json();
+})
+.then((data)=>{
+    console.log(data)
+})
+
+
+// fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/lateststart=1&limit=5000&convert=USD", {
+// //   body: "start=1&limit=5000&convert=USD",
+//   headers: {
+//     Accept: "application/json",
+//     // "Content-Type": "application/x-www-form-urlencoded",
+//     "X-Cmc_pro_api_key": "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c",
+//     'Access-Control-Allow-Origin':  'http://127.0.0.1',
+//     'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+//     // 'Access-Control-Allow-Credentials' : 'true',
+//     'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept'
+//   },
+// //   mode: "no-cors",
+// //   method: "GET"
+// }).then(response=>{
+//     console.log(response)
+// })
+
+   
 }
 
 fetchData();
